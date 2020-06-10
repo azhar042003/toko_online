e585ad27ffd183d883f3ef6441eb611b839d6a9c
azhar042003 <azhar042003@users.noreply.github.com>
2020-06-10 14:34:43 +0700

    Laporan hari ini : -Dalam perbaikan pada halaman login belum selesai dikarenakan masih ada yang error

--- application/controllers/change.php
+++ application/controllers/change.php
@@ -4,7 +4,7 @@ class Change extends CI_Controller{
 	public function index()
 	{
 		$data['title'] = 'Change Password';
-		$data['tb_user'] = $this->db->get_where('user', ['email' =>$this->session->userdata('nama')])->row_array();
+		$data['tb_user'] = $this->db->get_where('username', ['username' =>$this->session->userdata('nama')])->row_array();
 
 		$this->form_validation->set_rules('current_password', 'Current Password', 'required|trim');
 		$this->form_validation->set_rules('new_password1', 'New Password', 'required|trim|matches[new_password2]');
@@ -14,7 +14,7 @@ class Change extends CI_Controller{
 		if($this->form_validation->run() == false) {
 			$this->load->view('templates/header', $data);
 			$this->load->view('templates/sidebar', $data);
-			$this->load->view('auth/change', $data);
+			$this->load->view('templates/change', $data);
 			$this->load->view('templates/footer', $data);
 		} else {
 			$current_password = $this->input->post('current_password');
--- application/views/change.php
+++ application/views/change.php
@@ -1,14 +1,17 @@
-<div>
-  
+<body class="bg-gradient-primary">
 
+  <div class="container">
 
-  <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
-
-
-  <div class="row">
-      <div class="col-lg-6">
-        
-        <form action="<?= base_url('user/changepassword'); ?>" method="post">
+    <div class="card o-hidden border-0 shadow-lg col-lg-6 my-5">
+      <div class="card-body p-0">
+        <!-- Nested Row within Card Body -->
+          <div class="row">
+            <div class="col-lg">
+              <div class="p-5">
+            <div class="text-center">
+              <h1 class="h4 text-gray-900 mb-4">Ganti Password</h1>
+            </div>
+        <form method="post" action="<?php echo base_url('change/index') ?>" class="user">
           <div class="form-group">
             <label for="current_password">Current Password</label>
             <input type="password" class="form-control" id="current_password" name="current_password">
